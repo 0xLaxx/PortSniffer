c9a82b23b7a8334bab3f57b1ce8c85b974ed04ce
Christoph <frenzel96@gmail.com>
2020-04-04 19:31:36 +0200

    Berichtigung

--- PortSniffer/Program.cs
+++ PortSniffer/Program.cs
@@ -6,7 +6,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PortSniffer //Christoph stinkt und Kira ist cool bzw. die coolste
+namespace PortSniffer //Kira stinkt und total uncool bzw. die uncoolste
 {
     class Program
     {
