d29108986b5f791a88de25e63c7571c4c4f9c1db
Kira Kähler <kira.kaehler@web.de>
2020-04-04 19:27:19 +0200

    christoph stinkt immer noch

--- PortSniffer/Program.cs
+++ PortSniffer/Program.cs
@@ -6,7 +6,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PortSniffer //Christoph stinkt und Kira ist cool
+namespace PortSniffer //Christoph stinkt und Kira ist cool bzw. die coolste
 {
     class Program
     {
